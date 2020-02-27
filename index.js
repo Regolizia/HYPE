517d27d42d3b15fb9e3599222bb8228daecfb310
Eleonora Toscano <10536582@mail.polimi.it>
2020-02-27 20:11:48 +0100

    cleaning1

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.get('/index.html', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/travel_destination.html', function(req, res) {
+  res.sendFile(path.join(__dirname + '/travel_destination.html'));
+});
+
+app.get('/contact.html', function(req, res) {
+  res.sendFile(path.join(__dirname + '/contact.html'));
+});
+
+
+
+
