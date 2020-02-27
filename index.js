4db7aa738cfbe9889f522ea1f284db5f5380c697
Eleonora Toscano <10536582@mail.polimi.it>
2020-02-27 16:27:03 +0100

    provaViaggi2

--- index.js
+++ index.js
@@ -3,20 +3,10 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
-const urlShortener = require('node-url-shortener');
+const urlShortener = require('node-hyped');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded());
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
-
-app.post('/url', function(req, res) {
-  const url = req.body.url;
-
-  urlShortener.short(url, function(err, shortUrl){
-    res.send(shortUrl);
-  });
 });
